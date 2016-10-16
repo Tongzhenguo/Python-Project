83dcddae96f34835454e5e5988bd932cd61c70b4
Tongzhenguo <18210872892@163.com>
2016-10-16 11:11:15 +0800

    二叉树深度

---
+++ OJ/BinaryTreeDepth.py
@@ -0,0 +1,15 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if(root==None):
+            return 0
+        else:
+            return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
