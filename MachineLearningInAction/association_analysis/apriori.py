2ec912ff3c1b7ce007c66f1554efa158e8396178
admin <admin>
2016-10-29 18:33:32 +0800

    apriori

--- MachineLearningInAction/association_analysis/apriori.py
+++ MachineLearningInAction/association_analysis/apriori.py
@@ -14,7 +14,7 @@ def createcC1(dataSet):
     C1 = []
     for transaction in dataSet:
         for item in transaction:  ##python 不能创建只有一个整数的集合，因此这里实现必须使用列表
-            if item not in C1:
+            if [item] not in C1:  ## C1是1项集的集合
                 C1.append([item])
     C1.sort()
     return map(frozenset, C1)  ##对C1中每个项构建一个不变集合
@@ -31,21 +31,23 @@ def scanD(D, Ck, minSupport):
     for tid in D:
         for can in Ck:
             if (can.issubset(tid)):
-                if not ssCnt.has_key(can):
+                if (not ssCnt.has_key(can)):
                     ssCnt[can] = 1
                 else:
                     ssCnt[can] += 1
-        numItems = float(len(D))
-        retList = []
-        supportData = {}
-        for key in ssCnt:
-            support = ssCnt[key] / numItems  # 计算所有项集支持度
-            if (support >= minSupport):
-                retList.insert(0, key)
-            supportData[key] = support
-        return retList, supportData
+    numItems = float(len(D))
+    retList = []
+    supportData = {}
+    for key in ssCnt:
+        support = ssCnt[key] / numItems  # 计算所有项集支持度
+        if (support >= minSupport):
+            retList.insert(0, key)
+        supportData[key] = support
+    return retList, supportData
 
 
 dataSet = loadDataSet()
-C1 = createcC1(dataSet)
-print C1
+C1 = createcC1(dataSet)  ##1项集
+D = map(set, dataSet)  ##构建集合表示的数据集
+L1, supportData0 = scanD(D, C1, 0.5)  ##1频繁项集
+print L1
