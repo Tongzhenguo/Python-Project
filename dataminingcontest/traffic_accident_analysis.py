40a155fbb073e840476ea5247b068f2c06cf0598
admin <admin>
2016-11-02 23:02:23 +0800

    交通事故

--- dataminingcontest/traffic_accident_analysis.py
+++ dataminingcontest/traffic_accident_analysis.py
@@ -1,7 +1,31 @@
 # coding=utf-8
 tianqiFile = open("F:\code\Python-Project\dataset\wf1.csv")
 readlines = tianqiFile.readlines()
+wfxwcount = {}
 for line in readlines:
-    if (not line.__contains__("天气状况")):
+    if (not line.__contains__("wfxw")):
         ss = line.strip().replace("\t", ",").split(",")
-        print(ss[0])
+        if (wfxwcount.has_key(ss[3])):
+            wfxwcount[ss[3]] = wfxwcount[ss[3]] + 1
+        else:
+            wfxwcount[ss[3]] = 1
+sorted1 = sorted(wfxwcount.iteritems(), key=lambda p: p[1], reverse=True)
+for item in sorted1:
+    print item
+print len(wfxwcount)
+#
+# tianqiFile = open("F:\code\Python-Project\dataset\\accid1.csv")
+# readlines = tianqiFile.readlines()
+# wfxwcount = {}
+# for line in readlines:
+#     if (not line.__contains__("wfxw")):
+#         ss = line.strip().replace("\t", ",").split(",")
+#         colIDX = 1
+#         for colitem in ss:
+#             if(wfxwcount.has_key(ss[3])):
+#                 wfxwcount[ss[3]] = wfxwcount[ss[3]] +1
+#         else:
+#             wfxwcount[ss[3]] = 1
+# for item in wfxwcount.items():
+#     print item
+# print len(wfxwcount)
