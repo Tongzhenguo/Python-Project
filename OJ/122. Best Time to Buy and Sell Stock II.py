069c2e4d791f98ffe4ad5eff351360eebb83bded
Tongzhenguo <18210872892@163.com>
2017-02-13 14:28:24 +0800

    update

---
+++ OJ/122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+__author__ = 'arachis'
+
+"""
+Say you have an array for which the ith element is the price of a given stock on day i.
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times).
+However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+单股最大受益问题，可以多次交易，但只能全买或者全卖
+"""
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+
+        """
