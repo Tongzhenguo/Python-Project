6d6c09aac15a908750ed7eb29d244b260c71857d
Tongzhenguo <18210872892@163.com>
2017-02-14 17:57:01 +0800

    update

--- PlotEntropy.py
+++ PlotEntropy.py
@@ -1,5 +1,6 @@
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 
 __author__ = 'arachis'
 
@@ -10,11 +11,16 @@ def H(P):
         res = -res
         return res
 
+
 if __name__ == "__main__":
-    res = []
+    x = np.linspace(0,1,100)
+    # print x
+    y = np.empty_like(x)
     for i in range(1,51,1):
-        res.append( H([float(i)/100,1-float(i)/100]) )
+        y[i] =  H([float(i)/100,1-float(i)/100])
     for i in range(51,100,1):
-        res.append( H([1-float(i)/100,float(i)/100]) )
+        y[i] =  H([1-float(i)/100,float(i)/100])
+    # print y
+    plt.plot(x,y)
     plt.grid(True)
     plt.show()
