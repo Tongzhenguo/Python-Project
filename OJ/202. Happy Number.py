d247e7ddacefa4c4e0d67ff84bc4e5f8c193e868
Tongzhenguo <18210872892@163.com>
2017-02-16 15:38:08 +0800

    Hash

--- OJ/202. Happy Number.py
+++ OJ/202. Happy Number.py
@@ -15,19 +15,18 @@ Example: 19 is a happy number
 class Solution(object):
     def isHappy(self, n):
         """
-        N瞎给的，1000会超时
+        N是经验值
         """
         sumdigitS = 0
         N = 0
         while( n > 0 ):
             sumdigitS += ( n % 10 ) ** 2
-            print sumdigitS
             n /= 10
             if n == 0:
                 N += 1
                 n = sumdigitS
                 sumdigitS = 0
                 if n == 1:return True
-                if N == 100:return False
+                if N == 50:return False
 # print Solution().isHappy(19)
 # print Solution().isHappy(999)
