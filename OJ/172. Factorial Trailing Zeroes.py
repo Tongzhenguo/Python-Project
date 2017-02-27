285c059aa73bf4fe926e9e1a01ae374f55d9d839
Tongzhenguo <18210872892@163.com>
2017-02-27 15:41:44 +0800

    MATH 7

--- OJ/172. Factorial Trailing Zeroes.py
+++ OJ/172. Factorial Trailing Zeroes.py
@@ -11,18 +11,11 @@ class Solution(object):
         Because all trailing 0 is from factors 5 * 2.
         But sometimes one number may have several 5 factors,for example, 25 have two 5 factors, 125 have three 5 factors.
         In the n! operation, factors 2 is always ample(充足的). So we just count how many 5 factors in all number from 1 to n.
-
+        比如1 to 120 ,肯定有120/5个因子为5的因子，然后再去讨论可以被5整除两次，三次，。。。
         """
         if n <= 0:return 0
         else: # f(n) = n/5 + f(n/5)
-            #n / 5 + self.trailingZeroes(n / 5) #top1 solution
-            k = n - n%5 #my solution
-            c = 0
-            while(k % 5==0):
-                c += 1
-                k /= 5
-            print c
-            return c + self.trailingZeroes(n - n%5-5)
+            n / 5 + self.trailingZeroes(n / 5) #top1 solution
 
-print Solution().trailingZeroes(120) #28
+# print Solution().trailingZeroes(120) #28
 # print Solution().trailingZeroes(30)
