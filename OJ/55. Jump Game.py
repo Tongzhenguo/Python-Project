34c4ade01feea1eb9f7675dcc072d169f0a9633a
Tongzhenguo <18210872892@163.com>
2017-04-11 14:46:49 +0800

    update

---
+++ OJ/55. Jump Game.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+__author__ = 'arachis'
+"""
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+一个非负整数的数组，假设你的初始位置是A[0]，数组的每一个元素A[i]代表这你可走的最大步数，设计算法判断你是否可以到达A[n-1]
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false.
+"""
