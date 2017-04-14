136dfabc92cc373411c0e0e498e4aff3904f0c57
Tongzhenguo <18210872892@163.com>
2017-04-14 14:51:17 +0800

    update

---
+++ OJ/33. Search in Rotated Sorted Array.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+__author__ = 'arachis'
+"""
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+You may assume no duplicate exists in the array.
+假定一个升序的整型数组，在经过某种移动后，设计算法找到数组中的元素，如果不存在返回-1.假定数组不重复
+"""
---
+++ OJ/34. Search for a Range.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+__author__ = 'arachis'
+"""
+Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
+Your algorithm's runtime complexity must be in the order of O(log n).
+If the target is not found in the array, return [-1, -1].
+
+一个升序的整型数组，设计算法找到给定元素的开始和结束索引
+要去算法在O(logn),如果不存在返回【-1，-1】
+For example,
+Given [5, 7, 7, 8, 8, 10] and target value 8,
+return [3, 4].
+"""
